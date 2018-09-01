3f1f5ba93a9c5828358e9997ea4837099ea9f60b
fernanda <leonardo.mendes@4linux.com.br>
2018-09-01 17:31:34 -0300

    Classe de arquivo

--- index.php
+++ index.php
@@ -1,42 +1,80 @@
 <?php
+#criar um classe Arquivo
+#Que contenha 3 propriedades
+# - linhas - quantidade de linhas
+# - caminho - string com o caminhnho do arquivo
+# - conteudo - conteudo do arquivo
+# O método construtor deve receber o caminho do arquivo
+# Criar metodo abrir()
+# - chamar a funcao file() e jogar o conteudo de retorno na variavel conteudo
+# - aproveitar, e colocar na variavel linhas a quantidade de linhas do arquivo
+# Criar metodo escrever()
+# Criar metodo ler($i)
+# - ler a linha especificada
+# Criar metodo lerTudo()
+# - escrever as linhas do arquivo separando os valores por tabulacao
+# Criar metodo procurar()
 
+class Arquivo {
 
-class Animal {
-
-	public $especie = '';
-	public $peso = 0;
-	public $velocidade = 0;
-	public $fome = 0;
+	public $linhas = 0;
+	public $caminho = '';
+	public $conteudo = '';
 	
 
-
-	function __construct($especie = '', $peso = 0, $velocidade = 0, $fome = 0) {
-		$this->especie = $especie;
-		$this->peso = $peso;
-		$this->velocidade = $velocidade;
-		$this->fome = $fome;
-	}
-
-	function __toString(){
-		//return "Cachorro de raca $this->especie\n";
+	function __construct($caminho) {
+		#$this->linhas = $linhas;
+		$this->caminho = $caminho;
+		#$this->conteudo = $conteudo;
 		
-		$str = "Especie: $this->especie\n";
-		$str .= "Peso: $this->peso\n";
-		$str .= "Velocidade: $this->velocidade\n";
-		$str .= "Fome: $this->fome\n";
-		return $str;
-
-
 	}
 
+	function abrir(){
+		$this->conteudo = file($this->caminho);
+		$this->linhas = count($this->conteudo);
+	}
+
+	function lerTudo(){
+		echo "ID: \tNOME:\tEMAIL:\n";	
+		foreach ($usuarios as $usuario) {
+ 			list($id, $nome, $email) = explode (',' , $usuario);
+			echo "$id\t$nome\t$email";
+	}
 }
 
+	function ler($i){
+		if($i<= $this->linhas){
+			echo $this->conteudo[--$i];
+		}
+		else{
+			echo 'Linha nao existe';
+		}
+		#if(isset($this->conteudo[--$i]){
+		#echo implode("\t",explode('.', $this->conteudo[$i]));
+		#}
+	}
+
+
+	#function escrever(){
+
+#	}
+
+
 	
-$bulldog = new Animal('Bulldog', 18, 25, 2);
-echo $bulldog->toHTML();
+}
+
+$texto = new Arquivo ('usuarios.csv');
+$texto->abrir();
+$texto->ler(2);
 
 echo PHP_EOL;
 
-$dashround = new Animal('Dashround', 7, 27, 15);
-echo $dashround->toHTML();
+$palavra = new Arquivo ('lista.csv');
+$palavra->abrir();
+$palavra->ler(3);
+
+echo PHP_EOL;
+
+
+
 
